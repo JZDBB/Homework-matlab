1d2238d9d783f99b26c466f42a4ef86ce044524a
XuYining <oxuyining@gmail.com>
2018-11-11 22:27:35 +0800

    互相关函数 加噪声

--- StatisticalSignal.m
+++ StatisticalSignal.m
@@ -1,17 +1,17 @@
-% (1) plot x(t) and y(t)
+% (1) plot x(t) and y(t) with fs = 1ms
 w0 = 100 * pi;
 fs = 0.001;
 N = 100;
 t = 0:fs:(N-1)*fs;
 x = cos(w0 * t);
-y = cos(w0 * (t - 0.004));
+y = cos(w0 * (t - 4*fs));
 subplot(2, 1, 1);
-stem(t, x);
+stem(t, x, '.');
 xlabel('t');
 ylabel('x');
 title('x(t)');
 subplot(2, 1, 2);
-stem(t, y);
+stem(t, y, '.');
 xlabel('t');
 ylabel('y');
 title('y(t)');
@@ -19,10 +19,8 @@ title('y(t)');
 % (2) plot the histogram of Gaussian white noise n(t)
 mean = 0;
 sigma = 1;
-% n = wgn(1, 1000, 1);
-% n = normrnd(mean,sigma, 1000, 1);
 N = 1000;
-n = sqrt(sigma) * randn(N, 1);
+n = sqrt(sigma) * randn(1, N);
 figure, hist(n);
 xlabel('幅值');
 ylabel('频次');
@@ -31,8 +29,13 @@ title('高斯白噪声分布直方图');
 % (3) result
 [c, lags] = xcorr(x, y, 100);
 figure,
-stem(lags*0.001, c)
-fprintf('the Correlation of x(t), y(t) in t0= is%6.6f\n', c(4));
+stem(lags*0.001, c, '.')
+fprintf('the Correlation of x(t), y(t) without noise in t0= is%6.6f\n', c(4));
+
+[c_n, lags_n] = xcorr(x, y + n(1:100), 100);
+figure,
+stem(lags_n*0.001, c_n, '.')
+fprintf('the Correlation of x(t), y(t) added noise in t0= is%6.6f\n', c_n(4));
 
 
 
