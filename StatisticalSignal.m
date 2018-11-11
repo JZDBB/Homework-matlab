c9d3437bf2375e220ec8a32a34ca8e6bc10c9997
XuYining <oxuyining@gmail.com>
2018-11-11 22:16:04 +0800

    互相关函数 ok 需要debug

--- StatisticalSignal.m
+++ StatisticalSignal.m
@@ -2,16 +2,16 @@
 w0 = 100 * pi;
 fs = 0.001;
 N = 100;
-t = 0:fs:N*fs;
+t = 0:fs:(N-1)*fs;
 x = cos(w0 * t);
 y = cos(w0 * (t - 0.004));
 subplot(2, 1, 1);
-plot(t, x);
+stem(t, x);
 xlabel('t');
 ylabel('x');
 title('x(t)');
 subplot(2, 1, 2);
-plot(t, y);
+stem(t, y);
 xlabel('t');
 ylabel('y');
 title('y(t)');
@@ -21,7 +21,7 @@ mean = 0;
 sigma = 1;
 % n = wgn(1, 1000, 1);
 % n = normrnd(mean,sigma, 1000, 1);
-N = 100;
+N = 1000;
 n = sqrt(sigma) * randn(N, 1);
 figure, hist(n);
 xlabel('幅值');
@@ -29,10 +29,10 @@ ylabel('频次');
 title('高斯白噪声分布直方图');
 
 % (3) result
-c = xcorr(x,y);
-% printf(c(0.04));
+[c, lags] = xcorr(x, y, 100);
 figure,
-plot(c);
+stem(lags*0.001, c)
+fprintf('the Correlation of x(t), y(t) in t0= is%6.6f\n', c(4));
 
 
 
