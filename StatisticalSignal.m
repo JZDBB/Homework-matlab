c1bf569985310fe1e2727bad603414c1c8e91eec
XuYining <oxuyining@gmail.com>
2018-11-11 21:52:55 +0800

    高斯白噪声ok

--- StatisticalSignal.m
+++ StatisticalSignal.m
@@ -1,28 +1,39 @@
 % (1) plot x(t) and y(t)
 w0 = 100 * pi;
-t = 0:0.001:0.1;
+fs = 0.001;
+N = 100;
+t = 0:fs:N*fs;
 x = cos(w0 * t);
 y = cos(w0 * (t - 0.004));
 subplot(2, 1, 1);
 plot(t, x);
+xlabel('t');
+ylabel('x');
+title('x(t)');
 subplot(2, 1, 2);
 plot(t, y);
+xlabel('t');
+ylabel('y');
+title('y(t)');
 
 % (2) plot the histogram of Gaussian white noise n(t)
 mean = 0;
 sigma = 1;
-n = wgn(1, 1000, 1);
+% n = wgn(1, 1000, 1);
 % n = normrnd(mean,sigma, 1000, 1);
-% N = 1000;
-% n = sqrt(sigma) * randn(N, 1);
-
-%从高斯白噪声的统计信息和幅值分布看一下它的特点
-mean_value = mean(n);    %均值为0
-variance = var(n);      %方差为1，功率为0dBW（10*log1=0）
-figure, hist(noise);
+N = 100;
+n = sqrt(sigma) * randn(N, 1);
+figure, hist(n);
 xlabel('幅值');
 ylabel('频次');
-title('噪声幅值的直方图');
+title('高斯白噪声分布直方图');
+
+% (3) result
+c = xcorr(x,y);
+% printf(c(0.04));
+figure,
+plot(c);
+
 
 
 
