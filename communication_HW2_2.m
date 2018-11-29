620c92a740e3a4cb81d5127c968d5b1affb3a740
XuYining <oxuyining@gmail.com>
2018-11-29 21:39:14 +0800

    作业

--- communication_HW2_2.m
+++ communication_HW2_2.m
@@ -2,15 +2,16 @@ time = .3;
 Ts = 1/10000;
 t = Ts:Ts:time;
 lent = length(t);
-f0 = 1000;
+f0 = 1000; 
 A0 = 1;
-c = A0 * cos(2*pi*f0*t);
+c = A0 * cos(2*pi*f0*t + 3/2 * pi); % generate a carrier.
 fm = 100;
-w = cos(2*pi*fm*t);
-s = (w + c).^2;
+w = cos(2*pi*fm*t); % generate a signal bandwidth 100Hz.
+s = (w + c).^2; % add signal and carrier and square.
 v = (1/2) * bandp(s, 900, 1100, 800, 1200, 0.1, 30, 1/Ts);
+% use a bandpass filter to get v(t) to spread.
 
-
+% use AM.m to demodulate signal
 gamma = 0;
 phi = 0;
 c2 = cos(2*pi*(f0 + gamma)*t + phi);
@@ -20,15 +21,17 @@ damps = [1 1 0 0];
 fl = 100;
 b = remez(fl, fbe, damps);
 m = 2 * filter(b, 1, x);
-subplot(4, 1, 1);
+
+% plot
+subplot(3, 1, 1);
 plot(t, w);
 title('原始信号w(t)');
-subplot(4, 1, 2);
+subplot(3, 1, 2);
 plot(t, c);
 title('载波信号c(t)');
-subplot(4, 1, 3);
-plot(t, v)
-title('已调信号v(t) = {A_0}w(t)cos(2{\pi}{f_c}t)');
-subplot(4, 1, 4);
+subplot(3, 1, 3);
+% plot(t, v)
+% title('已调信号v(t) = {A_0}w(t)cos(2{\pi}{f_c}t)');
+% subplot(4, 1, 4);
 plot(t, m);
 title('解调信号m(t)');
--- communication_HW2_3.m
+++ communication_HW2_3.m
@@ -5,14 +5,15 @@ lent = length(t);
 fm1 = 20;
 fm2 = 10;
 m1 = cos(2*pi*fm1*t);
-m2 = cos(2*pi*fm2*t);
+m2 = cos(2*pi*fm2*t); % two signal with different frequence
 fc = 1000;
-phi1 = 0;
-phi2 = pi;
-c1 = cos(2*pi*fc*t + phi1);
-c2 = sin(2*pi*fc*t + phi2);
-v = m1.*c1 + m2.*c2;
+phi = pi;
+c10 = cos(2*pi*fc*t);
+c20 = sin(2*pi*fc*t); % two carrier with different phase
+v = m1.*c10 + m2.*c20; 
 
+c1 = cos(2*pi*fc*t + phi);
+c2 = sin(2*pi*fc*t + phi);
 x1 = v .* c1;
 x2 = v .* c2;
 fbe = [0 0.1 0.2 1];
@@ -22,29 +23,31 @@ b = remez(fl, fbe, damps);
 s1 = 2 * filter(b, 1, x1);
 s2 = 2 * filter(b, 1, x2);
 
-subplot(2, 1, 1);
+subplot(4, 1, 1);
 plot(t, m1);
-subplot(2, 1, 2);
+title('调制前{m_1}(t)')
+subplot(4, 1, 2);
 plot(t, m2);
+title('调制前{m_2}(t)')
+% figure,
+% subplot(2, 1, 1);
+% plot(t, c1);
+% subplot(2, 1, 2);
+% plot(t, c2);
+% 
+% figure,
+% plot(t, v);
+% 
+% figure,
+% subplot(2, 1, 1);
+% plot(t, x1);
+% subplot(2, 1, 2);
+% plot(t, x2);
 
-figure,
-subplot(2, 1, 1);
-plot(t, c1);
-subplot(2, 1, 2);
-plot(t, c2);
-
-figure,
-plot(t, v);
-
-figure,
-subplot(2, 1, 1);
-plot(t, x1);
-subplot(2, 1, 2);
-plot(t, x2);
-
-figure,
-subplot(2, 1, 1);
+% figure,
+subplot(4, 1, 3);
 plot(t, s1);
-subplot(2, 1, 2);
+title('解调后{s_1}(t), {\phi}={\pi}')
+subplot(4, 1, 4);
 plot(t, s2);
-
+title('解调后{s_2}(t), {\phi}={\pi}')
