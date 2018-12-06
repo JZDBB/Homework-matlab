1631d0ae31f0a6309945adbad99df973269b8f0c
XuYining <oxuyining@gmail.com>
2018-12-06 15:45:18 +0800

    test ifft

--- fft_test.m
+++ fft_test.m
@@ -1,4 +1,9 @@
 f = imread('airfield.bmp');
+img = f(1:8, 1:8);
+imwrite(img, '1.bmp');
+
+
+
 F = fft2(f);
 S = abs(F);
 imshow(S,[]);
@@ -8,3 +13,7 @@ imshow(Fc,[]);
 S2 = log(1+abs(Fc));
 figure,
 imshow(S2,[]);
+IF = ifft2(F);
+S3 = abs(IF);
+figure,
+imshow(IF, []);
